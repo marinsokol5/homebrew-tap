e1ec52fb9c6cd6862889db30dde4ebe99fddf728
Marin Sokol <marinsokol5@gmail.com>
2026-07-06 17:12:28 +0200

    brew-dummy 0.0.2

--- Casks/brew-dummy.rb
+++ Casks/brew-dummy.rb
@@ -1,6 +1,6 @@
 cask "brew-dummy" do
-  version "0.0.1"
-  sha256 "8c18ad8fa0eacc5fd29d6c6ad86c46ed70a9f9430dc50850e95fde11f22ad8c1"
+  version "0.0.2"
+  sha256 "f05cc568ad88368ae50c9d1578ea7481e5b1e3233c3da29313e32ffad04d7264"
 
   url "https://github.com/marinsokol5/brew-dummy/releases/download/v#{version}/brew-dummy-#{version}.zip"
   name "brew-dummy"
